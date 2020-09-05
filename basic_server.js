166b5bb48b7aadea3189e921396dc24e8049c46e
mudbungie <mudbungie@gmail.com>
2020-09-05 16:10:45 -0700

    protip, the & in your curl command will be caught by bash

---
+++ basic_server.js
@@ -0,0 +1,14 @@
+var http = require("http");
+var url = require("url");
+var dt = require('./dummy_module');
+
+http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write(dt.now())
+    console.log(request.url)
+    var q = url.parse(request.url, true)
+    console.log(q)
+    response.end('\n');
+}).listen(8081);
+
+console.log('Server running on localhost:8080');
